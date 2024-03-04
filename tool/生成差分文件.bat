9d44a12e3221cfa52d02335402c450db35a72cdb
DESKTOP-E89636G\ali213 <280606821@qq.com>
2024-03-04 11:52:10 +0800

    打包工具更新

--- tool/生成差分文件.bat
+++ tool/生成差分文件.bat
@@ -2,9 +2,9 @@ chcp 65001
 set /p var=旧包路径----
 
 set /p var2=新包路径---
-set /p var3=s输出路径---
+set /p var3=输出路径---
 
 
-java -Dfile.encoding=utf-8 -jar  "%cd%\jar\apkPactch.jar" -d '%var%' '%var2%' '%var3%' 
+java -Dfile.encoding=utf-8 -jar  "%cd%\jar\apkPactch.jar" -d '%var%' '%var2%' '%var3%'
 
 pause
