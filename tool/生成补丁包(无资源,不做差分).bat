0bb5e28f91d3fb491c80ff2e7515e14952899d5d
DESKTOP-E89636G\ali213 <280606821@qq.com>
2024-04-30 17:53:33 +0800

    修改打包工具

---
+++ tool/生成补丁包(无资源,不做差分).bat
@@ -0,0 +1,10 @@
+chcp 65001
+
+
+set /p var2=新包路径---
+set /p var3=输出路径---
+
+
+java -Dfile.encoding=utf-8 -jar  "%cd%\jar\apkPactch.jar" -g "" "%~dp0jar\baksmali\baksmali-2.4.0.jar" "%~dp0jar\baksmali\smali-2.4.0.jar"  "%var2%" "%var3%"
+
+pause
